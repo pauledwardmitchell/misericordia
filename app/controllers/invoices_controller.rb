17e9899ff249d90802fee2fc39e8dcb8f8a7686d
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-09-30 09:23:23 -0500

    Add functional oauth2 config for qbo

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,18 +2,47 @@ class InvoicesController < ApplicationController
   def index
   end
 
-  def authenticate
-    callback = quickbooks_oauth_callback_url
-    token = QB_OAUTH_CONSUMER.get_request_token(:oauth_callback => callback)
-    session[:qb_request_token] = token
-    redirect_to("https://appcenter.intuit.com/Connect/Begin?oauth_token=#{token.token}") and return
+  def oauth2
+    session[:state] = SecureRandom.uuid
+    # binding.pry
+    @client = Rack::OAuth2::Client.new(identifier: QBO_CLIENT_ID,
+                                       secret: QBO_CLIENT_SECRET,
+                                       redirect_uri: "http://localhost:3000/oauth2_redirect",
+                                       authorization_endpoint: "https://appcenter.intuit.com/connect/oauth2",
+                                       token_endpoint: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+                                     )
+    # binding.pry
+    # callback = quickbooks_oauth_callback_url
+    # token = QB_OAUTH_CONSUMER.get_request_token(:oauth_callback => callback)
+    # session[:qb_request_token] = token
+    # redirect_to("https://appcenter.intuit.com/Connect/Begin?oauth_token=#{token.token}") and return
   end
 
-  def oauth_callback
-  at = session[:qb_request_token].get_access_token(:oauth_verifier => params[:oauth_verifier])
-  token = at.token
-  secret = at.secret
-  realm_id = params['realmId']
-  # store the token, secret & RealmID somewhere for this user, you will need all 3 to work with Quickbooks-Ruby
+  def oauth2_redirect
+    # binding.pry
+    state = params[:state]
+    error = params[:error]
+    code = params[:code]
+    if state == session[:state]
+      client = Rack::OAuth2::Client.new(identifier: QBO_CLIENT_ID,
+                                       secret: QBO_CLIENT_SECRET,
+                                       redirect_uri: "http://localhost:3000/oauth2_redirect",
+                                       authorization_endpoint: "https://appcenter.intuit.com/connect/oauth2",
+                                       token_endpoint: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+                                     )
+      client.authorization_code = code
+      if resp = client.access_token!
+        session[:refresh_token] = resp.refresh_token
+        session[:access_token] = resp.access_token
+        session[:realm_id] = params[:realmId]
+        binding.pry
+        render 'index'
+      else
+        "Something went wrong. Try the process again"
+      end
+    else
+      "Error: #{error}"
+    end
   end
+
 end

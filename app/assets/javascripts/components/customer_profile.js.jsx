57f28958f9d0cdf8adedf1fa8c09a68e0f873094
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-10-18 19:53:44 -0500

    Add 30 days data

--- app/assets/javascripts/components/customer_profile.js.jsx
+++ app/assets/javascripts/components/customer_profile.js.jsx
@@ -6,7 +6,10 @@ const CustomerProfile = React.createClass({
       <section>
         <CustomerHeader customer={this.props.customer}/>
 
-        <CustomerRebateTracker customer_invoices={this.props.customer_invoices}/>
+        <CustomerRebateTracker
+          customer_invoices={this.props.customer_invoices}
+          total_thirty={this.props.total_thirty}
+          balance_thirty={this.props.balance_thirty}/>
 
       </section>
     )

980026c3f822c9f00223f7c49d63019e9b867569
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-11-13 14:36:14 -0600

    Add table to land show

--- app/assets/javascripts/components/landscaping_contract_details.jsx
+++ app/assets/javascripts/components/landscaping_contract_details.jsx
@@ -4,10 +4,24 @@ const LandscapingContractDetails = React.createClass({
 
     return (
       <section>
-        <h2>Landscaping Contract:</h2>
-        <section>Name: {this.props.contract.name}</section>
-        <section>CONTRACT ID: {this.props.contract.id}</section>
-        <section>Contract Dates: {this.props.contract.contract_start_date} to {this.props.contract.contract_end_date}</section>
+        <table style={{ width: '80%', marginLeft: '10%', marginRight: '10%', fontSize: 24, textAlign: 'center' }}>
+          <thead>
+            <tr>
+              <th>Contract #</th>
+              <th>Name</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{this.props.contract.id}</td>
+              <td>{this.props.contract.name}</td>
+              <td>{this.props.contract.contract_start_date}</td>
+              <td>{this.props.contract.contract_end_date}</td>
+            </tr>
+          </tbody>
+        </table>
 
         <a href={"/landscaping_contracts/" + this.props.contract.id + "/edit"}>Edit this contract</a>
 

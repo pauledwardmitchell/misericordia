69bf1c76da7bc954fe95c82f32b020ee0520f8b2
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-12-05 09:19:15 -0600

    Add task for updating elec and gas on buildings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201200644) do
+ActiveRecord::Schema.define(version: 20171204213036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20171201200644) do
     t.integer  "zip"
     t.integer  "square_footage"
     t.integer  "organization_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "annual_elec_usage"
+    t.integer  "annual_gas_usage"
   end
 
   create_table "cleaning_contracts", force: :cascade do |t|

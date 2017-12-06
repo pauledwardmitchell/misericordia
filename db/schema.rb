9519315fe433f91a3c7d5b8ca21d238e42cc5535
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-12-06 10:42:57 -0600

    Add load solar rake task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206154954) do
+ActiveRecord::Schema.define(version: 20171206162012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20171206154954) do
     t.boolean  "active",               default: true
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.float    "total_rebate_to_cpa"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -233,6 +234,7 @@ ActiveRecord::Schema.define(version: 20171206154954) do
     t.boolean  "active",               default: true
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.float    "total_rebate_to_cpa"
   end
 
   create_table "users", force: :cascade do |t|

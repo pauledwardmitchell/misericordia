41165fc76c47ae481b2388c3c00aaef1284aade7
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-12-07 17:47:44 -0600

    REturn ann rev methods to copier and solar

--- app/models/solar_contract.rb
+++ app/models/solar_contract.rb
@@ -1,4 +1,10 @@
 class SolarContract < ApplicationRecord
   has_many :solar_invoices
 
+  def annualized_revenue(year)
+    current_year_invoices = self.solar_invoices.select{ |i| i.current? }
+    total = current_year_invoices.map{ |i| i.amount }.reduce(:+)
+    total
+  end
+
 end

cc830416121824c73d713299ee1c27234a207af6
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-12-08 10:15:39 -0600

    Add tooltip with all category totals

--- app/assets/javascripts/components/single_rebate_row.jsx
+++ app/assets/javascripts/components/single_rebate_row.jsx
@@ -17,7 +17,19 @@ const SingleRebateRow = React.createClass({
     return (
       <tr>
         <td>{this.props.org.name}</td>
-        <td>$ {this.formatMoneyNumber(this.props.org.revenue)}</td>
+        <td className="tooltip">$ {this.formatMoneyNumber(this.props.org.revenue)}
+          <span className="tooltiptext">
+            <section>{this.props.org.name}</section>
+            <section>Landscaping: ${this.formatMoneyNumber(this.props.org.totals.landscaping_revenue)}</section>
+            <section>Electricity: ${this.formatMoneyNumber(this.props.org.totals.electricity_revenue)}</section>
+            <section>Gas: ${this.formatMoneyNumber(this.props.org.totals.gas_revenue)}</section>
+            <section>Cleaning: ${this.formatMoneyNumber(this.props.org.totals.cleaning_revenue)}</section>
+            <section>Security: ${this.formatMoneyNumber(this.props.org.totals.security_revenue)}</section>
+            <section>Waste: ${this.formatMoneyNumber(this.props.org.totals.waste_revenue)}</section>
+            <section>Solar: ${this.formatMoneyNumber(this.props.org.totals.solar_revenue)}</section>
+            <section>Copier: ${this.formatMoneyNumber(this.props.org.totals.copier_revenue)}</section>
+          </span>
+        </td>
         <td>{this.percentOfTotal()}%</td>
       </tr>
     )

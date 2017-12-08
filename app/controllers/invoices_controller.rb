9b79a9d445d66d600986da4cf4348867680d7093
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-12-07 19:19:59 -0600

    Debug addition of invoice arrays

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -208,10 +208,10 @@ class InvoicesController < ApplicationController
   def total_copier_revenue(year, array)
     all_invoices = []
     array.each do |contract|
-      all_invoices << contract.copier_invoices
+      all_invoices = all_invoices + contract.copier_invoices
       all_invoices
     end
-    current_invoices = all_invoices[0].select{ |i| i.current? }
+    current_invoices = all_invoices.select{ |i| i.current? }
     total = current_invoices.map{ |i| i.amount }.reduce(:+)
     total
   end
@@ -219,10 +219,10 @@ class InvoicesController < ApplicationController
   def total_solar_revenue(year, array)
     all_invoices = []
     array.each do |contract|
-      all_invoices << contract.solar_invoices
+      all_invoices = all_invoices + contract.solar_invoices
       all_invoices
     end
-    current_invoices = all_invoices[0].select{ |i| i.current? }
+    current_invoices = all_invoices.select{ |i| i.current? }
     total = current_invoices.map{ |i| i.amount }.reduce(:+)
     total
   end

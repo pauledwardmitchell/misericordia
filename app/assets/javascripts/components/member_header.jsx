c00b5a0aa2d6c18ab45a23f0a117ddf1ac8cfa60
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-12-13 14:46:36 -0600

    Add eligibility list to member show

--- app/assets/javascripts/components/member_header.jsx
+++ app/assets/javascripts/components/member_header.jsx
@@ -6,6 +6,15 @@ const MemberHeader = React.createClass({
     return pvrPercent
   },
 
+  sellList: function(sell_list) {
+    var string = "Eligible for Contracts in: "
+    for (i = 0; i < sell_list.length; i++) {
+      string += sell_list[i] + ", ";
+    }
+    string = string.slice(0, -2);
+    return string
+  },
+
   render: function() {
 
     return (
@@ -13,6 +22,9 @@ const MemberHeader = React.createClass({
         <h1 style={{textAlign: 'left', marginBottom: 10, marginLeft: '5%', fontSize: 40}}>{this.props.member_data.name}</h1>
         <section style={{textAlign: 'left', marginBottom: 40, marginLeft: '5%', fontSize: 28}}>
           <section>PVR: {this.formatPvr(this.props.member_data.pvr)}%</section>
+          <section>{this.props.member_data.institution_type}</section>
+          <section>{this.props.member_data.city_state}</section>
+          <section>{this.sellList(this.props.member_data.sell_list)}</section>
 
         </section>
       </section>

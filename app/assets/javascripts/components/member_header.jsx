6520e069db3c70325864e1aebf0dad17dbbd9ece
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-12-13 10:42:48 -0600

    Format pvr in view

--- app/assets/javascripts/components/member_header.jsx
+++ app/assets/javascripts/components/member_header.jsx
@@ -1,12 +1,19 @@
 const MemberHeader = React.createClass({
 
+  formatPvr: function(pvr) {
+    var pvrDecimal = pvr
+    var pvrPercent = (pvrDecimal.toFixed(2)*100)
+    return pvrPercent
+  },
+
   render: function() {
 
     return (
       <section>
         <h1 style={{textAlign: 'left', marginBottom: 10, marginLeft: '5%', fontSize: 40}}>{this.props.member_data.name}</h1>
-        <section style={{textAlign: 'left', marginBottom: 40, marginLeft: '5%', fontSize: 20}}>
-          PVR: {this.props.member_data.pvr}
+        <section style={{textAlign: 'left', marginBottom: 40, marginLeft: '5%', fontSize: 28}}>
+          <section>PVR: {this.formatPvr(this.props.member_data.pvr)}%</section>
+
         </section>
       </section>
     )

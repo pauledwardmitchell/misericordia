323e3d529f40443f6a83213f11be8e7673474620
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-01-08 15:46:36 -0600

    Add salt

--- app/models/qbo.rb
+++ app/models/qbo.rb
@@ -1,5 +1,5 @@
 class Qbo < ApplicationRecord
-  attr_encrypted :access_token, key: ENV["SECRET_KEY_BASE"]
-  attr_encrypted :refresh_token, key: ENV["SECRET_KEY_BASE"]
-  attr_encrypted :realm_id, key: ENV["SECRET_KEY_BASE"]
+  attr_encrypted :access_token, iv: SecureRandom.random_bytes(12), key: ENV["SECRET_KEY_BASE"]
+  attr_encrypted :refresh_token, iv: SecureRandom.random_bytes(12), key: ENV["SECRET_KEY_BASE"]
+  attr_encrypted :realm_id, iv: SecureRandom.random_bytes(12), key: ENV["SECRET_KEY_BASE"]
 end

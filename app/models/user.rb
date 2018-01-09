4af85a21d2a4a9ea715eb850117bc153d0385281
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-01-09 10:12:22 -0600

    Add validations, protect routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with Validator
+  validates :email, uniqueness: true, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end

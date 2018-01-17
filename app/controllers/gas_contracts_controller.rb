73e6e3d7e9f986ac6895ace1e06c7e12cc56ea06
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-01-17 09:44:15 -0600

    Add qbo logic

--- app/controllers/gas_contracts_controller.rb
+++ app/controllers/gas_contracts_controller.rb
@@ -15,22 +15,22 @@ class GasContractsController < ApplicationController
     @gas_contract = GasContract.find(params[:id])
     # @super_admin_emails = ['felipe@cpa.coop', 'jessica@cpa.coop', 'joe.naroditsky@cpa.coop', 'pauledwardmitchell@gmail.com']
     # if @super_admin_emails.include? current_user.email
-    #   qbo_api = QboApi.new(access_token: Qbo.first.access_token, realm_id: Qbo.first.realm_id)
-    #   qbo_api.class.production = true
+      qbo_api = QboApi.new(access_token: Qbo.first.access_token, realm_id: Qbo.first.realm_id)
+      qbo_api.class.production = true
 
-    #   @all_customers = qbo_api.all :customers
-    #   @all_non_member_customers =  @all_customers.reject {|c| c["DisplayName"][/^(Member -)/]}
-    # # end
-    # @customer_arrays = []
-    # @all_non_member_customers.each do |c|
-    #   @customer_array = [ c["DisplayName"], c["Id"]]
-    #   @customer_arrays << @customer_array
+      @all_customers = qbo_api.all :customers
+      @all_non_member_customers =  @all_customers.reject {|c| c["DisplayName"][/^(Member -)/]}
     # end
-    # @customer_arrays
-    @customer_arrays = [
-      ["Name 1", 1],
-      ["Name 2", 2]
-    ]
+    @customer_arrays = []
+    @all_non_member_customers.each do |c|
+      @customer_array = [ c["DisplayName"], c["Id"]]
+      @customer_arrays << @customer_array
+    end
+    @customer_arrays
+    # @customer_arrays = [
+    #   ["Name 1", 1],
+    #   ["Name 2", 2]
+    # ]
   end
 
   def update

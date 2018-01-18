c976c387fec07b88a0f2da0af23e16857a5a4f5d
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-01-18 15:26:29 -0600

    Hook up search to nav

--- app/assets/javascripts/components/global_search_box.jsx
+++ app/assets/javascripts/components/global_search_box.jsx
@@ -3,7 +3,7 @@ const GlobalSearchBox = React.createClass({
   render: function() {
 
     return (
-      <ul style={{border: 1, borderStyle: 'solid', position: 'absolute', top: 100}}>
+      <ul style={{border: 1, borderStyle: 'solid', position: 'absolute', top: 130}}>
         {this.props.customers
           .filter((customer) => `${customer.display_name}`.toUpperCase().indexOf(this.props.customerName.toUpperCase()) >= 0)
           .map((customer) => {
